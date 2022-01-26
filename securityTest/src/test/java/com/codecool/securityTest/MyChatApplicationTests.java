f2d35d026bbc8e5260a5bd1ae97c558873650741
Cozmin Alexandru Mlesnita <cozmin.mlesnita@gmail.com>
2022-01-26 12:42:29 +0200

    Initial commit

---
+++ securityTest/src/test/java/com/codecool/securityTest/MyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.securityTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
